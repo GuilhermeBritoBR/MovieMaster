a7221ff5e570cc2e1145d1cd3707e56d0ee3732d
Guilherme Santiago de Brito <guilhermesantiagodebritod.s@gmail.com>
2024-08-09 10:43:35 -0300

    Header Finalizado em partes

--- MovieMaster/App.js
+++ MovieMaster/App.js
@@ -12,7 +12,6 @@ import Inicio from './source/paginas/Incio.pagina';
   
 
 
-
 const Drawer = createDrawerNavigator();
 
 export default function App() {
@@ -20,14 +19,36 @@ export default function App() {
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="inicio"
       screenOptions={{
+        drawerInactiveTintColor: '#ffffff',
+        drawerActiveTintColor: '#ffffff',
         headerShown: false, 
+        drawerActiveBackgroundColor: '#1C1C1C',
+        drawerInactiveBackgroundColor: '#000000',
+        drawerStyle:{
+          backgroundColor: '#1C1C1C',
+        },
+        drawerLabelStyle: {
+          fontSize: 18, // Tamanho da fonte dos itens
+          fontWeight: 'regular', // Peso da fonte dos itens
+          color: '#ffffff', // Cor da fonte dos itens
+        },  
+      }}
+      >
+        <Drawer.Screen 
         
-      }}>
-        <Drawer.Screen name="Inicio" component={Inicio} />
+        name="Inicio" component={Inicio} />
         
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
+const EstilizaçãoBotoesDoMenu = StyleSheet.create(({
+  botãoAselecionar:{
+      borderBottomColor: '#ffffff',
+      borderBottomWidth: 1,
+      border: 'solid',
+      backgroundColor: '#ffffff',
+  }
+}))
 
